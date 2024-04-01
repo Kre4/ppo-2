f036350f144210a88ceb78808f5f06bed945ffd1
Kre4 <denhol2002@gmail.com>
2024-04-01 21:48:12 +0300

    change spring boot

--- bombardier-main/src/main/kotlin/com/itmo/microservices/demo/DemoServiceApplication.kt
+++ bombardier-main/src/main/kotlin/com/itmo/microservices/demo/DemoServiceApplication.kt
@@ -2,9 +2,11 @@ package com.itmo.microservices.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
 
 @SpringBootApplication
+@EnableAsync
 class DemoServiceApplication
 
 fun main(args: Array<String>) {
